2b059773f90756f58b275fd7ba2a37c7a4d73fca
webdev <web3dev8892@gmail.com>
2025-05-02 01:12:55 +0900

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ async function main() {
   const testEnv = process.env.TEST;
   console.log('TEST:', testEnv);
   console.log(process);
-  console.log('Hello World Test');
+  console.log('Hello World Test 1');
   while(true){
       await new Promise(resolve => setTimeout(resolve, 5000));
       console.log('Tick');
