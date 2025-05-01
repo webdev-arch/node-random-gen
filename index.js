0302778ba2ab379c39ce695193f651d3ce3a8388
webdev <web3dev8892@gmail.com>
2025-05-02 01:43:51 +0900

    complete test

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ require('dotenv').config();
 async function main() {
   const testEnv = process.env.TEST;
   console.log('TEST:', testEnv);
-  console.log(process);
-  console.log('Hello World Test 1');
+  console.log('Hello World Test 2');
   while(true){
       await new Promise(resolve => setTimeout(resolve, 5000));
       console.log('Tick');
